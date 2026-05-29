c6b9a807846888202f245f6e8461b6f3a4f7de91
smuneram98 <smm980531@gmail.com>
2026-05-29 18:42:42 -0500

    .

---
+++ Backend/notas/src/main/java/com/mayo/notas/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.mayo.notas.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mayo.notas.model.Post;
+
+public interface PostRepository extends JpaRepository<Post,Long>{
+    Optional<Post> findById(Long id);
+}
---
+++ Backend/notas/src/main/java/com/mayo/notas/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mayo.notas.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mayo.notas.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+    Optional<User> findByName(String username);
+}
